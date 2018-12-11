063acb7d7d41064c65f269b284909cfbaee9dbbf
alexnsouza6 <alexnsouza6@hotmail.com>
2018-12-11 16:11:07 -0200

    feat: new consts for actions

--- src/actions/consts.js
+++ src/actions/consts.js
@@ -1,3 +1,5 @@
 export const USER_LOGIN = 'user_login';
+export const USER_LOGOUT = 'user_logout';
 export const USER_CREATE = 'user_create';
+export const USER_EDITED = 'user_edited';
 export const URL = 'http://localhost:8080';
