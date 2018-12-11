7094a7a59a39bbf9733cc217bf6d3954a04d4669
alexnsouza6 <alexnsouza6@hotmail.com>
2018-12-11 16:11:29 -0200

    feat: user redux state after login/logout

--- src/reducers/user/userReducer.js
+++ src/reducers/user/userReducer.js
@@ -1,11 +1,13 @@
-import { USER_CREATE } from '../../actions/consts';
+import { USER_CREATE, USER_EDITED } from '../../actions/consts';
 
 const initialState = {};
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
     case USER_CREATE:
-      return { ...state, ...payload };
+      return { ...state, user: { payload }, loggedIn: true };
+    case USER_EDITED:
+      return { ...state, user: { payload }, loggedIn: true };
     default:
       return state;
   }
