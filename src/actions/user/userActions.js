b91a0a9f683f12c63d90e14eed5e2af5934dd042
alexnsouza6 <alexnsouza6@hotmail.com>
2018-12-11 16:12:15 -0200

    feat: Actions for editing user info

--- src/actions/user/userActions.js
+++ src/actions/user/userActions.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import { URL, USER_CREATE } from '../consts';
+import { toastr } from 'react-redux-toastr';
+import { URL, USER_CREATE, USER_EDITED } from '../consts';
 
 export const signUpUser = userInfo => dispatch => {
   axios({
@@ -8,6 +9,22 @@ export const signUpUser = userInfo => dispatch => {
     data: userInfo,
     headers: { contentType: 'application/json' },
   })
-    .then(response => dispatch({ payload: response.data, type: USER_CREATE }))
+    .then(response => {
+      dispatch({ payload: response.data, type: USER_CREATE });
+      toastr.success('Welcome to DinnerDash', 'Enjoy your Meal!');
+    })
     .catch(error => console.log(error.response.data.message));
+};
+
+export const editUser = (userInfo, userId) => dispatch => {
+  const userToken = localStorage.getItem('userToken');
+  axios({
+    method: 'patch',
+    url: `${URL}/users/${userId}`,
+    data: userInfo,
+    headers: { contentType: 'application/json', authorization: `Bearer ${userToken}` },
+  }).then(response => {
+    dispatch({ payload: response.data, type: USER_EDITED });
+    toastr.success('User Info Updated');
+  });
 };
