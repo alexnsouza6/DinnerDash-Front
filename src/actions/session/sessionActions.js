2699b8063a11ee8582e045982a50a744f512450c
alexnsouza6 <alexnsouza6@hotmail.com>
2018-12-11 16:10:07 -0200

    feat: logout feature

--- src/actions/session/sessionActions.js
+++ src/actions/session/sessionActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import { URL, USER_LOGIN } from '../consts';
+import { toastr } from 'react-redux-toastr';
+import { URL, USER_LOGIN, USER_LOGOUT } from '../consts';
+import { history } from '../../index';
 
 export const authUser = userInfo => dispatch => {
   axios({
@@ -8,6 +10,26 @@ export const authUser = userInfo => dispatch => {
     data: userInfo,
     headers: { contentType: 'application/json' },
   })
-    .then(response => dispatch({ payload: response.data, type: USER_LOGIN }))
-    .catch(error => console.log(error.response.data.message));
+    .then(response => {
+      toastr.success('Welcome to DinnerDash', 'Enjoy your Meal!');
+      dispatch({ payload: response.data, type: USER_LOGIN });
+      console.log(response.data);
+      history.push(`/profile/edit/${response.data.id}`);
+    })
+    .catch(error => {
+      if (error.response && error.response.data) console.log(error.response.data.message);
+    });
+};
+
+export const logoutUser = user => dispatch => {
+  axios({
+    method: 'delete',
+    url: `${URL}/logout`,
+    data: user,
+    headers: { contentType: 'application/json', authorization: `Bearer ${user.token}` },
+  }).then(() => {
+    toastr.success('Bye bye!', 'Get Back Soon!');
+    dispatch({ payload: {}, type: USER_LOGOUT });
+    history.push('/');
+  });
 };
--- src/reducers/session/sessionReducer.js
+++ src/reducers/session/sessionReducer.js
@@ -1,4 +1,4 @@
-import { USER_LOGIN } from '../../actions/consts';
+import { USER_LOGIN, USER_LOGOUT } from '../../actions/consts';
 
 const initialState = {};
 
@@ -6,8 +6,10 @@ export default (state = initialState, { type, payload }) => {
   switch (type) {
     case USER_LOGIN:
       localStorage.setItem('userToken', JSON.stringify(payload.token));
-      return { ...state, ...payload };
-
+      return { ...state, user: payload, loggedIn: true };
+    case USER_LOGOUT:
+      localStorage.removeItem('userToken');
+      return { ...state, user: payload, loggedIn: false };
     default:
       return state;
   }
