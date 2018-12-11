d1edb2977016bef6e977707de8a89b2d2a5a09de
alexnsouza6 <alexnsouza6@hotmail.com>
2018-12-11 16:09:25 -0200

    feat: adding redux toastr

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React, { Fragment } from 'react';
 import { Route } from 'react-router-dom';
+import ReduxToastr from 'react-redux-toastr';
 import LandingPage from './components/LandingPage';
-import EditForm from './containers/EditForm';
+import AccessPage from './containers/AccessPage';
 
 const App = () => (
   <Fragment>
-    <Route path="/profile/edit" component={EditForm} />
+    <Route path="/profile" component={AccessPage} />
     <Route exact path="/" component={LandingPage} />
+    <ReduxToastr
+      timeOut={4000}
+      newestOnTop={false}
+      preventDuplicates
+      position="top-left"
+      transitionIn="fadeIn"
+      transitionOut="fadeOut"
+      progressBar
+      closeOnToastrClick
+    />
   </Fragment>
 );
 
