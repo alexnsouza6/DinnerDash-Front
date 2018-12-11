d4edc9c552fe14d3a9bd3904bca507ba68f10231
alexnsouza6 <alexnsouza6@hotmail.com>
2018-12-11 16:11:16 -0200

    feat: redux toastr reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer } from 'redux-form';
+import { reducer as toastrReducer } from 'react-redux-toastr';
 import sessionReducer from './session/sessionReducer';
 
 export default combineReducers({
   form: reducer,
   login: sessionReducer,
+  toastr: toastrReducer,
 });
