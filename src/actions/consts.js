d5ffab1957b14e8a64ecfbe373a7ec14abc89e36
alexnsouza6 <alexnsouza6@hotmail.com>
2018-12-15 21:36:25 -0200

    feat: URL for production

--- src/actions/consts.js
+++ src/actions/consts.js
@@ -2,4 +2,4 @@ export const USER_LOGIN = 'user_login';
 export const USER_LOGOUT = 'user_logout';
 export const USER_CREATE = 'user_create';
 export const USER_EDITED = 'user_edited';
-export const URL = 'http://localhost:8080';
+export const URL = 'https://dinnerdash-api.herokuapp.com/';
